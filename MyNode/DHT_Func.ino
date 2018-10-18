739c551cda5f7b273784efb340fb4181ac1af8b2
your name <brian7lan3@gmail.com>
2018-10-18 11:36:48 +0800

    自己面試版

--- MyNode/DHT_Func.ino
+++ MyNode/DHT_Func.ino
@@ -2,7 +2,7 @@
 //extern float humidity;
 
 
-#define DHTPIN 4     // what digital pin we're connected to
+#define DHTPIN 4     // PORT:D2
 #define DHTTYPE DHT11   // DHT 11
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -32,7 +32,7 @@ void setupDHT() {
 
 void loopDHT() {
   // Wait a few seconds between measurements.
-  delay(2000);
+//  delay(2000);
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -53,7 +53,7 @@ void loopDHT() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-  loopPS();
+  //  loopPS();
 
   Serial.print("Humidity: ");
   Serial.print(h);
--- MyNode/MyNode.ino
+++ MyNode/MyNode.ino
@@ -2,11 +2,14 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <aREST.h>
+#define DHTPIN 2  //PORT:D4
 float temperature;
 float humidity;
-int photoresistor;
-void setup() {
 
+int photoresistor;
+
+void setup() {
+  pinMode(2, OUTPUT);
   // Start Serial
   Serial.begin(115200);
 
@@ -14,14 +17,97 @@ void setup() {
 
   setupARest();
 
+
 }
 
 void loop() {
 
   loopDHT();
   //  loopDHT_Fake();
+  loopPS();
 
   loopARest();
   uploadData();
+  //  loopLED();
+
+  if (temperature < 30 && humidity < 60)
+  {
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
+  }
+  else if (temperature > 30 && humidity < 60)
+  {
+    digitalWrite(2, HIGH);
+    delay(250);
+    digitalWrite(2, LOW);
+    delay(250);
+    digitalWrite(2, HIGH);
+    delay(250);
+    digitalWrite(2, LOW);
+    delay(250);
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
+  }
+  else if (temperature < 30 && humidity > 60)
+  {
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(250);
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(250);
+    digitalWrite(2, HIGH);
+    delay(250);
+    digitalWrite(2, LOW);
+    delay(250);
+  }
+  else {
+
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+    digitalWrite(2, HIGH);
+    delay(125);
+    digitalWrite(2, LOW);
+    delay(125);
+
+
+  }
+
 
 }
